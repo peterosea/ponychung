97dd7a94d98cfe675364e3325fdb846a47bfd50a
Hansanghyeon <999@hyeon.pro>
2021-04-02 12:32:43 +0900

    :sparkles: 지원현황 > 접수현황 > 접수현황 리스트 & 접수현황리스트 > 지원자 리스트 & 지원자리스트 상세

--- pages/register-list.php
+++ pages/register-list.php
@@ -15,26 +15,50 @@ $urlParent = '../';
   <div class="container con_width flex_col">
     <div class="row">
       <div class="filter_wrap">
-        <div class="article_head article_title">
-          <div class="row inner">
-            <div class="col col-lg-8 d-flex align-items-center">
-              <span class="label _ing">모집중</span>
-              <h2 class="_title">2020 제5회 민연 젊은 한국학 아카데미 온라인 개최 안내</h2>
+        <div class="row align-items-end">
+          <div class="col col-lg-8 flex_row filter_grp justify-content-start flex-wrap">
+            <div class="dropdown py-2">
+              <button class="btn btn-secondary dropdown-toggle w170" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                타입
+              </button>
+              <ul class="dropdown-menu w170" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">전체</a></li>
+                <li><a class="dropdown-item" href="#">학술지원</a></li>
+                <li><a class="dropdown-item" href="#">장학</a></li>
+              </ul>
             </div>
-            <div class="col col-lg-4 flex-wrap">
-              <div class="_classify mb-2">국내장학금 / 포니정해외학술탐방단</div>
-              <div class="_period">2021.02.28 - 2021.03.28</div>
+            <!-- 타입: 장학 -->
+            <div class="dropdown py-2">
+              <button class="btn btn-secondary dropdown-toggle w170" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                구분 1
+              </button>
+              <ul class="dropdown-menu w170" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">전체</a></li>
+                <li><a class="dropdown-item" href="#">국내장학슴</a></li>
+                <li><a class="dropdown-item" href="#">베트남 장학금</a></li>
+                <li><a class="dropdown-item" href="#">특별 장학금</a></li>
+                <li><a class="dropdown-item" href="#">신진연구자지원</a></li>
+                <li><a class="dropdown-item" href="#">한국학 지원구분</a></li>
+              </ul>
             </div>
-          </div>
-        </div>
-        <div class="row align-items-center">
-          <div class="col col-lg-7 flex_row filter_grp">
+            <!-- 타입: 학술지원  -->
+            <!-- <div class="dropdown py-2">
+              <button class="btn btn-secondary dropdown-toggle w170" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                구분 2
+              </button>
+              <ul class="dropdown-menu w170" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">전체</a></li>
+                <li><a class="dropdown-item" href="#">포니정해외 학술탐방단</a></li>
+                <li><a class="dropdown-item" href="#">포니정인문연구장학</a></li>
+                <li><a class="dropdown-item" href="#">기존장학</a></li>
+              </ul>
+            </div> -->
             <div class="search_box input-group py-2 w320">
               <input type="text" class="form-control" placeholder="지원자명을 입력해주세요" aria-label="검색어를 입력해주세요" aria-describedby="button-addon2">
               <button class="btn btn-outline-secondary" type="button" id="button-addon2">검색</button>
             </div>
           </div>
-          <div class="col col-lg-5 flex_row justify-content-end">
+          <div class="col col-lg-4 flex_row justify-content-end">
             <div class="btn excel my-2">엑셀다운로드</div>
             <div class="dropdown py-2">
               <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
@@ -54,81 +78,66 @@ $urlParent = '../';
           <thead>
             <tr>
               <th>No.</th>
-              <th>이름</th>
-              <th>성별</th>
-              <th>학사</th>
-              <th>학위</th>
-              <th>수료일</th>
-              <th>학교</th>
-              <th>분약</th>
-              <th>추천서<br />(첨부파일)</th>
-              <th>성적</th>
+              <th>타입</th>
+              <th>구분</th>
+              <th>상태</th>
+              <th>제목</th>
+              <th>지원자 수</th>
+              <th>공고 기간</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td class="_num">4</td>
-              <td class="_name">
-                <a href="/pages/register-single.php">김지원</a>
+              <td>
+                학술지원
               </td>
-              <td class="_gender">남</td>
-              <td class="_bachelor">카이스트</td>
-              <td class="_bachelor">석사</td>
-              <td class="_completion">2019.01</td>
-              <td class="_school">서울대</td>
-              <td class="_powdered_medicine">미약</td>
-              <td class="_attachment">
-                <a href="#"></a>
+              <td>한국학 지원</td>
+              <td>모집중</td>
+              <td class="_title">
+                <a href="/pages/register-list-id.php">
+                  2020 제5회 민연 젊은 한국학 아카데미 온라인 개최 안내
+                </a>
               </td>
-              <td class="_score">3.9/4.5</td>
+              <td>10</td>
+              <td class="_period">
+                2021-01-01 ~ 2021-01-31
+              </td>
             </tr>
             <tr>
               <td class="_num">3</td>
-              <td class="_name">
-                <a href="/pages/register-single.php">이지원</a>
+              <td>
+                장학
               </td>
-              <td class="_gender">남</td>
-              <td class="_bachelor">연세대</td>
-              <td class="_bachelor">석사</td>
-              <td class="_completion">2019.01</td>
-              <td class="_school">이대</td>
-              <td class="_powdered_medicine">여성학</td>
-              <td class="_attachment">
-                <a href="#"></a>
+              <td>국내장학금 - 포니정해외학술탐방단</td>
+              <td>심사중</td>
+              <td class="_title">
+                <a href="/pages/register-list-id.php">
+                  2020 포니정재단 학술지원 연구과제 공모 (+FAQ)
+                </a>
               </td>
-              <td class="_score">3.9/4.5</td>
+              <td>24</td>
+              <td class="_period">
+                2021-01-01 ~ 2021-01-31
+              </td>
             </tr>
             <tr>
               <td class="_num">2</td>
-              <td class="_name">
-                <a href="/pages/register-single.php">박지원</a>
+              <td>
+                학술지원
               </td>
-              <td class="_gender">남</td>
-              <td class="_bachelor">서울대</td>
-              <td class="_bachelor">석사</td>
-              <td class="_completion">2019.01</td>
-              <td class="_school">연세대</td>
-              <td class="_powdered_medicine">문헌정보학</td>
-              <td class="_attachment">
-                <a href="#"></a>
+              <td>신진연구자 지원
               </td>
-              <td class="_score">3.9/4.5</td>
-            </tr>
-            <tr>
-              <td class="_num">1</td>
-              <td class="_name">
-                <a href="/pages/register-single.php">최지원</a>
+              <td>심사중</td>
+              <td class="_title">
+                <a href="/pages/register-list-id.php">
+                  [공지] 2020 포니정 인문연구장학 모집 공고
+                </a>
               </td>
-              <td class="_gender">남</td>
-              <td class="_bachelor">한예종</td>
-              <td class="_bachelor">석사</td>
-              <td class="_completion">2019.01</td>
-              <td class="_school">경희대</td>
-              <td class="_powdered_medicine">독일근대사</td>
-              <td class="_attachment">
-                <a href="#"></a>
+              <td>18</td>
+              <td class="_period">
+                2021-01-01 ~ 2021-01-31
               </td>
-              <td class="_score">3.9/4.5</td>
             </tr>
           </tbody>
         </table>
