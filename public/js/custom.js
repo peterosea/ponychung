7cb60a0798cb22211e9bdfec1f6a25dba2ba12ae
Hansanghyeon <999@hyeon.pro>
2021-04-08 15:22:45 +0900

    :bug: 햄버거 메뉴 스크롤 시, 스크롤바 생기면서 추가로 계속 스크롤되는 현상이 있음. 수정

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,28 +1,29 @@
-$(document).ready(function(){
-    $('.slider-for').slick({
-        dots: false,
-        infinite: true,
-        fade: true,
-        cssEase: 'linear',
-        arrows:false
-    });
-    $('.slider-nav').slick({
-        asNavFor: '.slider-for',
-        dots: false,
-        infinite: true,
-        focusOnSelect: true,
-        cssEase: 'linear',
-        arrows:false,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        variableWidth: true,
-    });
+$(document).ready(function () {
+  $(".slider-for").slick({
+    dots: false,
+    infinite: true,
+    fade: true,
+    cssEase: "linear",
+    arrows: false,
+  });
+  $(".slider-nav").slick({
+    asNavFor: ".slider-for",
+    dots: false,
+    infinite: true,
+    focusOnSelect: true,
+    cssEase: "linear",
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+  });
 
-    $('.mobile_menu').click(function(){
-        $(this).toggleClass("_active");
-        $('.mobile_nav').toggleClass("_active");
-    });
-    $('.submenu_grp').hover(function(){
-        $('nav.main_nav').toggleClass("_active");
-    });
+  $(".mobile_menu").click(function () {
+    $(this).toggleClass("_active");
+    $(".mobile_nav").toggleClass("_active");
+    $("body").toggleClass("mobileMenuActive");
+  });
+  $(".submenu_grp").hover(function () {
+    $("nav.main_nav").toggleClass("_active");
+  });
 });
